3c6ee863f4827b67c843f60c5ede432caf3bea7d
Kevin Keller <flygeneticist@gmail.com>
2013-07-26 21:36:51 -0400

    added table limit validation for wagers + tweaked misc layout

--- public/misc.js
+++ public/misc.js
@@ -1,10 +1,17 @@
 function validateWagerForm()
 {
 var bankroll=document.forms["newround_wager"]["secret-bankroll"].value;
+var minbid=document.forms["newround_wager"]["minbid"].value;
+var maxbid=document.forms["newround_wager"]["maxbid"].value;
 var wager=document.forms["newround_wager"]["wager"].value;
 if (wager==null || wager=="" || wager==0 || (bankroll-wager) < 0)
   {
   alert("Wager must not be blank, zero, or greater than your bankroll!");
+  return false;
+  }
+if ((wager-maxbid) > 0 || (wager-minbid) < 0)
+  {
+  alert("Wager must be within the table limits!");
   return false;
   }
   return true;
@@ -15,6 +22,8 @@ function validateNewPlayerForm()
 var bankroll=document.forms["gamesettings"]["bankroll"].value;
 var player=document.forms["gamesettings"]["player"].value;
 var wager=document.forms["gamesettings"]["wager"].value;
+var minbid=document.forms["gamesettings"]["minbid"].value;
+var maxbid=document.forms["gamesettings"]["maxbid"].value;
 if (player==null || player=="")
   {
   alert("Please enter a player name!");
@@ -23,6 +32,11 @@ if (player==null || player=="")
 if (wager==null || wager=="" || wager==0 || (bankroll-wager) < 0)
   {
   alert("Wager must not be blank, zero, or greater than your bankroll!");
+  return false;
+  }
+if ((wager-maxbid) > 0 || (wager-minbid) < 0)
+  {
+  alert("Wager must be within the table limits!");
   return false;
   }
   return true;
